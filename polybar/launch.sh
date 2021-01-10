8ac363a964011eaebb785f404c6a7b2983ef64b8
nicomazz <nicomazz97@gmail.com>
2021-01-10 20:13:03 +0100

    Fix polybar - make it work even if screens change names

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
-polybar bottom;
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+
+MONITORS=$MONITORS polybar top &
+MONITOR=$MONITORS polybar bottom;
 
 echo "Bars launched..."
